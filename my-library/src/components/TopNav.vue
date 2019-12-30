aae47b99778714a0922c31868df1e29b7d860430
Jeffrey Vaz <jeff.j.vaz@gmail.com>
2019-12-30 10:56:49 -0500

    add top and bottom nav bar layout

---
+++ my-library/src/components/TopNav.vue
@@ -0,0 +1,64 @@
+<template>
+  <nav>
+    <v-app-bar
+      app
+      flat
+      color="primary"
+      dark
+    >
+      <v-toolbar
+        flat
+        color="primary" 
+      >
+        <v-toolbar-title
+        >
+          <span>Spartan</span>
+          <span class="font-weight-medium">Library</span>
+        </v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <div class="text-center">
+          <v-menu 
+            offset-y
+            origin="top center"
+            transition="scale-transition"
+          >
+            <template 
+              v-slot:activator="{ on }"
+            >
+              <v-btn 
+                v-on="on"
+                text
+              >
+                <v-icon>expand_more</v-icon>
+                <span>get started</span>
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item>
+                <v-list-item-title>My Library</v-list-item-title>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-title>About</v-list-item-title>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-title>Contact</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </div>
+
+      </v-toolbar>
+    </v-app-bar>
+
+  </nav>
+</template>
+
+<script>
+
+export default {
+  name: 'TopNav',
+
+}
+</script>
