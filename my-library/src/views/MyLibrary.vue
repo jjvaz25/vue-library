20e6497da688cc6f96d6da623cc7c53a0ed3ca5b
Jeffrey Vaz <jeff.j.vaz@gmail.com>
2019-12-30 14:14:36 -0500

    add proper spacing between cards in My Library

--- my-library/src/views/MyLibrary.vue
+++ my-library/src/views/MyLibrary.vue
@@ -2,52 +2,59 @@
   <div class="my-library">
     <h1 class="title grey--text text--darken-1">My Library</h1>
 
-    <v-container class="my-5">
-
-      <v-card 
-        v-for="item in library" :key="item.title"
-        class="mx-auto"
-        max-width="344"
-        outlined
-      >
-        <v-list-item three-line>
-          <v-list-item-content>
-            <div>
-              <div v-if="item.completed" class="overline mb-4">Completed</div>
-              <div v-else class="overline mb-4">Incomplete</div>
-            </div>
-            <v-list-item-title class="headline mb-1">{{ item.title }}</v-list-item-title>
-            <v-list-item-subtitle>{{ item.creator }}</v-list-item-subtitle>
-            <v-list-item-subtitle>{{ item.rating }}/10</v-list-item-subtitle>
-          </v-list-item-content>
-
-          <v-list-item-avatar
-            tile
-            size="80"
-            color="grey lighten-1"
+    <v-container fluid class="my-5">
+      <v-row>    
+        <v-col 
+          v-for="item in library" :key="item.title"
+          cols="12" lg="4" md="6" s="12"
+          align="start"
+          
+        >
+          <v-card 
+            class="mx-auto"
+            max-width="330"
+            outlined
           >
-            <img v-if="item.category === 'Video Game'"
-              src='../assets/videogame.png'
-            >
-            <img v-else-if="item.category === 'Book'"
-              src='../assets/book.png'
-            >
-            <img v-else
-              src='../assets/icon.png'
-            >
-          </v-list-item-avatar>
+            <v-list-item three-line>
+              <v-list-item-content>
+                <div>
+                  <div v-if="item.completed" class="overline mb-4">Completed</div>
+                  <div v-else class="overline mb-4">Incomplete</div>
+                </div>
+                <v-list-item-title class="headline mb-1">{{ item.title }}</v-list-item-title>
+                <v-list-item-subtitle>{{ item.creator }}</v-list-item-subtitle>
+                <v-list-item-subtitle>{{ item.rating }}/10</v-list-item-subtitle>
+              </v-list-item-content>
 
-        </v-list-item>
+              <v-list-item-avatar
+                tile
+                size="80"
+                color="grey lighten-1"
+              >
+                <img v-if="item.category === 'Video Game'"
+                  src='../assets/videogame.png'
+                >
+                <img v-else-if="item.category === 'Book'"
+                  src='../assets/book.png'
+                >
+                <img v-else
+                  src='../assets/icon.png'
+                >
+              </v-list-item-avatar>
 
+            </v-list-item>
 
-        <v-card-actions>
-          <v-btn >Edit</v-btn>
-          <v-btn >Delete</v-btn>
-        </v-card-actions>
+            <v-card-actions>
+              <v-btn>Completd</v-btn>
+              
+              <v-spacer></v-spacer>
+              <v-btn >Edit</v-btn>
+              <v-btn >Delete</v-btn>
+            </v-card-actions>
 
-      </v-card>
-      
-
+          </v-card>
+        </v-col>
+      </v-row>
     </v-container>
 
   </div>
