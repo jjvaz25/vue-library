0dffa406eca8bb12b704f35561ab29c759579f0f
Jeffrey Vaz <jeff.j.vaz@gmail.com>
2019-12-30 13:42:24 -0500

    add rough outline of cards in library

--- my-library/src/views/MyLibrary.vue
+++ my-library/src/views/MyLibrary.vue
@@ -1,6 +1,55 @@
 <template>
   <div class="my-library">
-    <h1>My Library</h1>
+    <h1 class="title grey--text text--darken-1">My Library</h1>
+
+    <v-container class="my-5">
+
+      <v-card 
+        v-for="item in library" :key="item.title"
+        class="mx-auto"
+        max-width="344"
+        outlined
+      >
+        <v-list-item three-line>
+          <v-list-item-content>
+            <div>
+              <div v-if="item.completed" class="overline mb-4">Completed</div>
+              <div v-else class="overline mb-4">Incomplete</div>
+            </div>
+            <v-list-item-title class="headline mb-1">{{ item.title }}</v-list-item-title>
+            <v-list-item-subtitle>{{ item.creator }}</v-list-item-subtitle>
+            <v-list-item-subtitle>{{ item.rating }}/10</v-list-item-subtitle>
+          </v-list-item-content>
+
+          <v-list-item-avatar
+            tile
+            size="80"
+            color="grey lighten-1"
+          >
+            <img v-if="item.category === 'Video Game'"
+              src='../assets/videogame.png'
+            >
+            <img v-else-if="item.category === 'Book'"
+              src='../assets/book.png'
+            >
+            <img v-else
+              src='../assets/icon.png'
+            >
+          </v-list-item-avatar>
+
+        </v-list-item>
+
+
+        <v-card-actions>
+          <v-btn >Edit</v-btn>
+          <v-btn >Delete</v-btn>
+        </v-card-actions>
+
+      </v-card>
+      
+
+    </v-container>
+
   </div>
 </template>
 
@@ -10,6 +59,17 @@
 export default {
   name: 'MyLibrary',
   components: {
+  },
+  data() {
+    return {
+      library: [
+        { title: 'The Last of Us', creator: 'Naughty Dog', category: 'Video Game', completed: true, rating: 8 },
+        { title: 'Metal Gear Solid V', creator: 'Konami', category: 'Video Game', completed: true, rating: 9 },
+        { title: 'My Brilliant Friend', creator: 'Elena Ferrante', category: 'Book', completed: true, rating: 8 },
+        { title: 'Zen and the Art of Motorcycle Maintenance', creator: 'Robert M. Pirsig', category: 'Book', completed: false, rating: 3 },
+        { title: 'The Odin Project', creator: 'Open Source', category: 'Other', completed: false, rating: 10 },
+      ]
+    }
   }
 }
 </script>
